bdd8e614e9a1c6e6a9e99cd4a48ba3d9b81883b6
Marcos Vinicius <viniciushp00@gmail.com>
2023-11-02 16:21:37 -0300

    Primeira commit do projeto

---
+++ app/src/main/java/com/example/ideiaprojeto/MateriaViewHolder.java
@@ -0,0 +1,15 @@
+package com.example.ideiaprojeto;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MateriaViewHolder extends RecyclerView.ViewHolder {
+
+
+
+    public MateriaViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
---
+++ app/src/main/java/com/example/ideiaprojeto/criacaodenovamateria.java
@@ -0,0 +1,14 @@
+package com.example.ideiaprojeto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class criacaodenovamateria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criacaodenovamateria);
+    }
+}
