42a4ed31a5d726aa6a7b0342b6ef05baf75cf3ba
Marcos Vinicius <viniciushp00@gmail.com>
2023-11-23 23:10:39 -0300

    Configuração da tela de calculo

---
+++ app/src/main/java/com/example/ideiaprojeto/Adapter/AdapaterNotas.java
@@ -0,0 +1,59 @@
+package com.example.ideiaprojeto.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ideiaprojeto.R;
+import com.example.ideiaprojeto.model.Atividade;
+import com.example.ideiaprojeto.model.Materia;
+
+public class AdapaterNotas extends RecyclerView.Adapter<AdapaterNotas.NotasViewHolder> {
+
+    Materia materia;
+
+    public AdapaterNotas(Materia materia){
+        this.materia = materia;
+    }
+
+    @NonNull
+    @Override
+    public NotasViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.notas,parent,false);
+        NotasViewHolder holder = new NotasViewHolder(itemLista);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NotasViewHolder holder, int position) {
+
+        Atividade atividade = materia.getAtividades().get(position);
+        holder.nmAtividade.setText(atividade.getNome());
+        holder.ntValor.setText(String.valueOf(atividade.getNota()));
+        holder.atividade = atividade;
+    }
+
+    @Override
+    public int getItemCount() {
+        return materia.getAtividades().toArray().length;
+    }
+
+    public class NotasViewHolder extends  RecyclerView.ViewHolder {
+
+        Atividade atividade;
+        TextView nmAtividade = itemView.findViewById(R.id.nmAtividade);
+
+        EditText ntValor = itemView.findViewById(R.id.ntValor);
+
+
+        public NotasViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
--- app/src/main/java/com/example/ideiaprojeto/calculo.java
+++ app/src/main/java/com/example/ideiaprojeto/calculo.java
@@ -1,8 +1,15 @@
 package com.example.ideiaprojeto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.example.ideiaprojeto.Adapter.AdapaterAtividades;
+import com.example.ideiaprojeto.Adapter.AdapaterNotas;
+import com.example.ideiaprojeto.model.Materia;
+import com.example.ideiaprojeto.state.GerenciamentoEstados;
 
 public class calculo extends AppCompatActivity {
 
@@ -10,5 +17,18 @@ public class calculo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculo);
+
+        Materia materia = GerenciamentoEstados.atualMateria;
+
+        RecyclerView recycleView_atividades = findViewById(R.id.recyclerviewAtividadeCalc);
+        recycleView_atividades.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+
+        //Para dar mais desempenho a lista
+        recycleView_atividades.setHasFixedSize(true);
+
+        //Configuração do Adapter
+        AdapaterNotas adapter = new AdapaterNotas(materia);
+        recycleView_atividades.setAdapter(adapter);
+
     }
 }
